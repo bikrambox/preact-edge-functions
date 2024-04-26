b5ffa0a96e0ac6a58919f5febd4664845527f672
Bikram rongpi <bikramrongpi088@gmail.com>
2024-04-26 13:57:40 +0530

    Initial commit

--- src/app.jsx
+++ src/app.jsx
@@ -5,7 +5,6 @@ import First from './components/first';
 const App = () => {
   return (
 <>
-<Head/>
 <body>
 <First />
 </body>
@@ -15,29 +14,5 @@ const App = () => {
 
 
 
-function Head() {
-  return (
-    <head>
-      <title>Preact on the edge</title>
-      <meta charset="utf-8" />
-      <link rel="icon" href="/static/favicon.ico" />
-      <link rel="stylesheet" href="/static/app.css" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta
-        name="description"
-        content="HTML, dynamically rendered in a city near you"
-      />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@vercel" />
-      <meta name="twitter:creator" content="@vercel" />
-      <meta name="twitter:title" content="Preact on the edge" />
-      <meta
-        name="twitter:description"
-        content="HTML, dynamically rendered in a city near you"
-      />
-      <meta name="twitter:image" content="/static/og/card.png" />
-      <meta name="twitter:image:alt" content="The Vercel and Preact logos" />
-    </head>
-  )
-}
+
 export default App;
