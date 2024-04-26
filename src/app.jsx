0fcf97f6c4ae3ce459748b7fa46198fec015dbef
Bikram rongpi <bikramrongpi088@gmail.com>
2024-04-26 13:13:41 +0530

    Start

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+// src/App.js
+import { h } from 'preact';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, Preact!</h1>
+      <p>This is a simple Preact application.</p>
+    </div>
+  );
+};
+
+export default App;
